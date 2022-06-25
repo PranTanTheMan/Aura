75d439418b6fbfcb25c5eb16ba95fce69e88ffd0
jmurphy5613 <jmurphy5613@gmail.com>
2022-06-25 12:59:54 -0400

    set up react-router

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,13 @@
+import './navbar.css';
+
+
+const Navbar = () => {
+    return (
+        <div className="root">
+            <img className="logo" src="/aura hckthn 2.png" alt="" />
+            <h1 className="name">Aura</h1>
+        </div>
+    )
+}
+
+export default Navbar;
